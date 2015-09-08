7bd41a56b102dc83535e1a0e1d34b9af7a08e748
Lisa Ridley <lisa@prometsource.com>
2015-09-07 23:15:36 -0400

    Adding patches plugin, sfr, composer, drupal-dirs.sh

--- build/drupal-dirs.sh
+++ build/drupal-dirs.sh
@@ -11,33 +11,12 @@ if [ -d www/sites/all/modules/features ]
 then
   rm -Rf www/sites/all/modules/features
 fi
-cp -Rf modules/features www/sites/all/modules/features
+cp -Rf modules/feature_modules www/sites/all/modules/features
 
-if [ ! -d www/sites/all/themes ]
-then
-  mkdir www/sites/all/themes
-fi
-if [ ! -d www/sites/default/themes ]
-then
-  mkdir www/sites/default/themes
-fi
 #Recreate the themes custom folder
 if [ -d www/sites/all/themes/custom ]
 then
   rm -Rf www/sites/all/themes/custom
 fi
 
-cp -Rf themes/custom/ www/sites/default/themes/
-
-#Recreate the libraries custom folder
-if [ -d www/sites/all/libraries/custom ]
-then
-  rm -Rf www/sites/all/libraries/custom
-fi
-if [ ! -d www/sites/all/libraries ]
-then
-  mkdir www/sites/all/libraries
-fi
-cp -Rf libraries/custom www/sites/all/libraries/custom
-
-cp settings/settings.php www/sites/default/settings.php
+cp -Rf themes/custom/ www/sites/all/themes/custom
